c3b686967089303807509877e5e402a4033d9266
sy-andou <kekeru.the.solo@gmail.com>
2022-03-12 14:26:37 +0900

    Loadingアイコン

---
+++ components/Loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="loading">
+    <img class="loading__icon" src="/img/loading-icon.svg" alt="ローディング" />
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__icon {
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
