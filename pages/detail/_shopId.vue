f6115cffb499b47f209416155836f441fcb3f3ed
sy-andou <kekeru.the.solo@gmail.com>
2022-04-10 20:42:12 +0900

    誤字修正

--- pages/detail/_shopId.vue
+++ pages/detail/_shopId.vue
@@ -73,7 +73,7 @@ export default {
         user_id: this.$auth.user.id,
         shop_id: this.$route.params.shopId,
       };
-      await this.$axios.post("https://resebackend.herokuapp.com/api/favorite/", sendData);
+      await this.$axios.post("https://resebackend.herokuapp.com/api/favorite", sendData);
       alert('お気に入りに追加しました。');
       this.$store.dispatch("shops/getShopsData");
     },
